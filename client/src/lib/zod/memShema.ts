268cd50bc3e64fda600e9647d42009394daf94e9
Alexahder Kochish <shvepsolek@gmail.com>
2025-04-17 22:42:32 +0100

    make three edits fields require

--- client/src/lib/zod/memShema.ts
+++ client/src/lib/zod/memShema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const memSchema = z.object({
-  title: z.string().min(3).max(100).optional(),
+  title: z.string().min(3).max(100),
   image: z
     .string()
     .trim()
@@ -9,8 +9,8 @@ export const memSchema = z.object({
     .refine((val) => !val || /^https?:\/\/.+\.jpg$/.test(val), {
       message: "Image must be a valid JPG URL",
     }),
-  desc: z.string().max(200).optional(),
-  likes: z.number().int().min(0).max(99).optional(),
+  desc: z.string().max(200),
+  likes: z.number().int().min(0).max(99),
 });
 
 export type memSchemaType = z.infer<typeof memSchema>;
